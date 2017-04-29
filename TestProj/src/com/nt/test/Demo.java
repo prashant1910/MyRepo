0035140fed131fce9a9d93e2fb61db557dcafef4
PRASHANT PC <PRASHANT PC@192.168.0.9>
2017-04-29 23:29:13 +0530

    kept by dev2

--- TestProj/src/com/nt/test/Demo.java
+++ TestProj/src/com/nt/test/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world 0000");
+		System.out.println("hello world 000011111111");
 
 	}
 
